2b50bc781e32d24f68e309e8df15436a2aa2d3fe
QLWeilcf <1226824818@qq.com>
2019-08-31 16:32:52 +0800

    Add files via upload

---
+++ storylineWithCluster/storymap-markers.js
@@ -0,0 +1,7 @@
+var markers = {
+    beijing: {lat:39.886426, lon: 116.404762, zoom: 6},
+	tianjin: {lat:39.134594, lon: 117.191961, zoom: 7},
+	fuyang: {lat:32.645140, lon: 116.268333, zoom: 7},
+	ningbo: {lat:29.763531, lon: 121.898233, zoom: 8},
+	liuzhou: {lat:24.313703, lon: 109.406884, zoom: 7}
+};
